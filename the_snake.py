c35bf3b50797f21661af977eb651bdd4357d6882
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2024-11-06 15:09:05 +0500

    feat: implement move method to snake class

--- the_snake.py
+++ the_snake.py
@@ -12,6 +12,8 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
+DIRECTION = [UP, DOWN, LEFT, RIGHT]
+
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 
 BORDER_COLOR = (93, 216, 228)
@@ -30,6 +32,8 @@ clock = pygame.time.Clock()
 
 
 class GameObject:
+    PAUSED = False
+
     def __init__(self) -> None:
         self.position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         self.body_color = None
@@ -42,17 +46,21 @@ class Apple(GameObject):
     def __init__(self):
         super().__init__()
         self.body_color = APPLE_COLOR
-        self.position = self.random_position()
 
     def draw(self):
         rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, rect)
         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
-    def random_position(self):
-        random_x = choice(range(0, SCREEN_WIDTH, GRID_SIZE))
-        random_y = choice(range(0, SCREEN_HEIGHT, GRID_SIZE))
-        return (random_x, random_y)
+    def random_position(self, snake_positions: list[tuple[int, int]]) -> None:
+        while True:
+            random_x = choice(range(0, SCREEN_WIDTH, GRID_SIZE))
+            random_y = choice(range(0, SCREEN_HEIGHT, GRID_SIZE))
+            position = (random_x, random_y)
+
+            if position not in snake_positions:
+                self.position = position
+                return None
 
 
 class Snake(GameObject):
@@ -60,7 +68,10 @@ class Snake(GameObject):
         super().__init__()
         self.body_color = SNAKE_COLOR
         self.positions = [self.position]
+        self.direction = choice(DIRECTION)
         self.last = None
+        self.length = 0
+        self.next_direction = None
 
     def draw(self):
         for position in self.positions[:-1]:
@@ -68,7 +79,7 @@ class Snake(GameObject):
             pygame.draw.rect(screen, self.body_color, rect)
             pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
-        head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
+        head_rect = pygame.Rect(self.get_head_position(), (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, head_rect)
         pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
 
@@ -76,8 +87,28 @@ class Snake(GameObject):
             last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
             pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
 
+    def get_head_position(self):
+        return self.positions[0]
 
-def handle_keys(game_object):
+    def move(self):
+        position_x, position_y = self.position
+        next_position_x, next_position_y = self.direction
+        self.position = (
+            (position_x + (next_position_x * GRID_SIZE)) % SCREEN_WIDTH,
+            (position_y + (next_position_y * GRID_SIZE)) % SCREEN_HEIGHT,
+        )
+        self.positions.insert(0, self.position)
+        if len(self.positions) - 1 > self.length:
+            self.last = self.positions.pop()
+        else:
+            self.last = None
+
+    def update_direction(self):
+        if self.next_direction:
+            self.direction = self.next_direction
+
+
+def handle_keys(game_object: Snake):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -97,12 +128,19 @@ def main():
     pygame.init()
     apple = Apple()
     snake = Snake()
-
     while True:
         clock.tick(SPEED)
-        handle_keys(apple)
+        handle_keys(snake)
         apple.draw()
         snake.draw()
+        if not snake.PAUSED:
+            snake.move()
+        snake.update_direction()
+
+        if apple.position in snake.positions:
+            snake.length += 1
+            apple.random_position(snake.positions)
+
         pygame.display.update()
 
 
